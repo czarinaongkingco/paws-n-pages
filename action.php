1e26c1139149b4a6017f3b19a168dd021e6dab77
czarinaongkingco <77886936+czarinaongkingco@users.noreply.github.com>
2023-07-18 04:51:04 +0800

    added for deleting of pet

--- action.php
+++ action.php
@@ -1,29 +1,42 @@
 <?php
-    include('connection.php');
-    include('config.php');
+include('connection.php');
+include('config.php');
 
-    ///////////////////// FOR DELETING PRODUCT ////////////////////////////  
-    if (isset($_POST['delete_product'])) {
-        $Supply_ID = $_POST['Supply_ID'];
-        $dsql = mysqli_query($con, "DELETE FROM petsupplies WHERE SupplyID= '$Supply_ID'");
-        if ($dsql) {
-            echo 200;
-        } else {
-            echo "<script>alert('Error deleting data.');</script>";
-            echo "<script>document.location = 'supplies.php'</script>";
-        }
+///////////////////// FOR DELETING PRODUCT ////////////////////////////  
+if (isset($_POST['delete_product'])) {
+    $Supply_ID = $_POST['Supply_ID'];
+    $dsql = mysqli_query($con, "DELETE FROM petsupplies WHERE SupplyID= '$Supply_ID'");
+    if ($dsql) {
+        echo 200;
+    } else {
+        echo "<script>alert('Error deleting data.');</script>";
+        echo "<script>document.location = 'supplies.php'</script>";
     }
+}
 
-    ///////////////////// FOR DELETING SERVICES ////////////////////////////  
-    if (isset($_POST['delete_service'])) {
-        $Service_ID = $_POST['Service_ID'];
-        $dsql = mysqli_query($con, "DELETE FROM services WHERE ServiceID= '$Service_ID'");
-        if ($dsql) {
-            echo 200;
-        } else {
-            echo "<script>alert('Error deleting data.');</script>";
-            echo "<script>document.location = 'supplies.php'</script>";
-        }
-    } 
-    
+///////////////////// FOR DELETING SERVICES ////////////////////////////  
+if (isset($_POST['delete_service'])) {
+    $Service_ID = $_POST['Service_ID'];
+    $dsql = mysqli_query($con, "DELETE FROM services WHERE ServiceID= '$Service_ID'");
+    if ($dsql) {
+        echo 200;
+    } else {
+        echo "<script>alert('Error deleting data.');</script>";
+        echo "<script>document.location = 'supplies.php'</script>";
+    }
+}
+
+///////////////////// FOR DELETING PET OWNER PROFILE ////////////////////////////  
+if (isset($_POST['delete_user'])) {
+    $User_ID = $_POST['User_ID'];
+    $adsql = mysqli_query($con, "DELETE FROM address WHERE UserID='$User_ID'");
+    $dsql = mysqli_query($con, "DELETE FROM users WHERE UserID='$User_ID'");
+    if ($dsql && $adsql) {
+        echo 200;
+    } else {
+        echo "<script>alert('Error deleting data.');</script>";
+        echo "<script>document.location = 'userprofile.php'</script>";
+    }
+}
+
 ?>
