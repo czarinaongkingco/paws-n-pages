eef7c21536ffb7c7bf1d92ad170a0571cc7bb520
czarinaongkingco <77886936+czarinaongkingco@users.noreply.github.com>
2023-07-18 01:29:14 +0800

    fixed error if invalid credentials

--- login.php
+++ login.php
@@ -12,7 +12,8 @@ include('connection.php');
 <head>
     <meta charset="utf-8">
     <title>Paws N Pages | Login </title>
-    <link rel="icon" href="https://media.discordapp.net/attachments/1112075552669581332/1113455947420024832/icon.png" type="image/x-icon">
+    <link rel="icon" href="https://media.discordapp.net/attachments/1112075552669581332/1113455947420024832/icon.png"
+        type="image/x-icon">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Free HTML Templates" name="keywords">
     <meta content="Free HTML Templates" name="description">
@@ -82,7 +83,8 @@ include('connection.php');
                 <div class="col-md-4">
                     <form method="post" action="">
                         <div class="row g-3 bg-light" style=" border-radius: 15px; padding:30px 30px;">
-                            <center><a href="index.php"><img src="img/logo_black.png" style="height:150px; width:150px;"></a></center>
+                            <center><a href="index.php"><img src="img/logo_black.png"
+                                        style="height:150px; width:150px;"></a></center>
 
                             <?php
 
@@ -105,6 +107,8 @@ include('connection.php');
                                             echo '<div class="alert alert-danger" style="border-radius:15px;"><i class="fa fa-times-circle"></i>&nbsp; Invalid Username or Password!</div>';
                                         }
                                     }
+                                } else {
+                                    echo '<div class="alert alert-danger" style="border-radius:15px;"><i class="fa fa-times-circle"></i>&nbsp; Invalid Username or Password!</div>';
                                 }
                             }
 
@@ -122,11 +126,15 @@ include('connection.php');
 
 
                             <div class="col-12">
-                                <input type="text" name="username" id="username" class="form-control  bg-light border-3 px-4 py-3" style="border-radius:15px;" placeholder="Username">
+                                <input type="text" name="username" id="username"
+                                    class="form-control  bg-light border-3 px-4 py-3" style="border-radius:15px;"
+                                    placeholder="Username">
                             </div>
                             <div class="col-12">
                                 <div class="password-container">
-                                    <input type="password" name="password" id="password" class="form-control  bg-light border-3 px-4 py-3" style="border-radius:15px;" placeholder="Password">
+                                    <input type="password" name="password" id="password"
+                                        class="form-control  bg-light border-3 px-4 py-3" style="border-radius:15px;"
+                                        placeholder="Password">
                                     <i class="fa-solid fa-eye" id="eye"></i>
                                 </div>
                             </div>
@@ -134,10 +142,12 @@ include('connection.php');
                                 <a href="forgot_password.php">Forgot Password?</a>
                             </div>
                             <div class="col-12">
-                                <button type="submit" name="submit" class="btn btn-primary w-100 py-3" style="border-radius:15px;">Submit</button>
+                                <button type="submit" name="submit" class="btn btn-primary w-100 py-3"
+                                    style="border-radius:15px;">Submit</button>
                             </div>
                             <center>
-                                <div class="col-12" style="padding-top:30px;">Don't have an account? <a href="vet-or-pet.php">Sign Up</a></div>
+                                <div class="col-12" style="padding-top:30px;">Don't have an account? <a
+                                        href="vet-or-pet.php">Sign Up</a></div>
                             </center>
 
                         </div>
@@ -193,7 +203,7 @@ include('connection.php');
         const passwordInput = document.querySelector("#password");
         const eye = document.querySelector("#eye");
 
-        eye.addEventListener("click", function() {
+        eye.addEventListener("click", function () {
             this.classList.toggle("fa-eye-slash")
             const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
             passwordInput.setAttribute("type", type)
