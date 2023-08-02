9f47adf885457154400789f09ee82dd7090f45eb
czarinaongkingco <77886936+czarinaongkingco@users.noreply.github.com>
2023-08-02 11:56:54 +0000

    corrected spelling of august

--- orders_admin.php
+++ orders_admin.php
@@ -97,7 +97,7 @@ $clinicID = $row_ca['ClinicID'];
                 return n;
             }
 
-            var months = ["January", "February", "March", "April", "May", "June", "July", "Augest", "September", "October", "November", "December"];
+            var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
             var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
             var ids = ["dayname", "month", "daynum", "year", "hour", "minutes", "seconds", "period"];
             var values = [week[dname], months[mo], dnum.pad(2), yr, hou.pad(2), min.pad(2), sec.pad(2), pe];
